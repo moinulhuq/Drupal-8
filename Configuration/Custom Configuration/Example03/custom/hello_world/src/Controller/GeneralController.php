1ca58f125b303d0a2026c22e6d61732615491898
moin <moinulhuq@gmail.com>
2018-03-13 10:05:32 +1300

    Built in configuration change

---
+++ Configuration/Custom Configuration/Example03/custom/hello_world/src/Controller/GeneralController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\hello_world\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class GeneralController.
+ */
+class GeneralController extends ControllerBase {
+
+  /**
+   * Hello.
+   *
+   * @return array
+   *   Return Hello string.
+   */
+  public function hello(){
+
+      // Built in configuration "System maintanance messgae" change
+      $config = \Drupal::service('config.factory')->getEditable('system.maintenance');
+      $config->set('message', 'hello')->save();
+      $message = $config->get('message');
+
+      // Built in configuration "System performance cache max_age" change
+      $config = \Drupal::service('config.factory')->getEditable('system.performance');
+      $config->set('cache.page.max_age', 1)->save();
+      $message = $config->get('cache.page.max_age');
+
+      return [
+          '#type' => 'markup',
+          '#markup' => $message,
+      ];
+  }
+}
