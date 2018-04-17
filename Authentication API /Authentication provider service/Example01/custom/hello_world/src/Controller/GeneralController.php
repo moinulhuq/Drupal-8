374725e9c8a8894cbadab92b2a8b22b10875d83c
moin <moinulhuq@gmail.com>
2018-04-18 07:54:22 +1200

    Custom Authentication Service Provider

---
+++ Authentication API /Authentication provider service/Example01/custom/hello_world/src/Controller/GeneralController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\hello_world\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class GeneralController.
+ */
+class GeneralController extends ControllerBase {
+
+  /**
+   * Hello.
+   *
+   * @return array
+   *   Return Hello string.
+   */
+  public function hello() {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Custom Authentication Service Provider'),
+    ];
+  }
+
+}
